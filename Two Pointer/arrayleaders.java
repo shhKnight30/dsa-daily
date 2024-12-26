8d5c26141fbbbe5298c8b0962f169362c7a8e3c2
shhKnight30 <aishwarybatt@gmail.com>
2024-12-26 14:12:37 +0530

    array leaders problem solved

---
+++ Two Pointer/arrayleaders.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = arr.length-1;
+        int max = arr[i];
+        list.add(max);
+        i--;
+        while(i>=0){
+            if(arr[i]>m=ax){
+                list.add(0,arr[i]);
+                max = arr[i];
+                
+            }
+            i--;
+        }
+        return list;
+    }
+}
